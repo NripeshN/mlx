bfad73d8183954c36716ed321aed641501b6bbf8
Goni Zahavy <goni1993@gmail.com>
2026-03-08 00:23:32 +0200

    added build-venv.sh to setup venv

---
+++ build-venv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+uv venv --clear --seed --python 3.13 venv
+uv pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm7.2
+uv pip install mlx-lm mlx-vlm
