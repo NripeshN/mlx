60e0f5e3e782f000d0bb39f63da1c15df01db45a
Goni Zahavy <goni1993@gmail.com>
2026-03-22 03:25:38 +0200

    add param to benchmark_qwen3.sh

--- benchmark_qwen3.sh
+++ benchmark_qwen3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+quant=${1-bf16}
+
 # Disable OpenMPI ROCm accelerator to prevent segfault on exit
 export OMPI_MCA_accelerator=^rocm
 
 source venv/bin/activate
-mlx_lm.benchmark --model mlx-community/Qwen3-0.6B-bf16 -p 4096 -g 128
+mlx_lm.benchmark --model mlx-community/Qwen3-0.6B-$quant -p 4096 -g 128
